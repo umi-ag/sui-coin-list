c4687268450832035b756a1e9a9f91b106676246
fuyutarow <fuyutarow@gmail.com>
2022-11-27 09:15:18 +0900

    [ts] Modify CoinProfile

--- typescript/src/coinList.ts
+++ typescript/src/coinList.ts
@@ -7,8 +7,11 @@ export type CoinProfile = {
   officialSymbol: string
   decimals: number
   logoURI: string
-  package: HexString
-  objects?: Record<string, HexString>
+  objects: {
+    package: string
+    module: string
+    TreasuryCap: string
+  }
   extensions: {
     binanceSymbol?: string
     coingeckoId?: string
@@ -32,8 +35,9 @@ export const coinlist: CoinProfile[] = [
     officialSymbol: 'BTC',
     decimals: 8,
     logoURI: 'https://assets.coingecko.com/coins/images/1/small/bitcoin.png?1547033579',
-    package: "0x4e58cae8dca973030dd84459bdf9cd70e65dcc80",
     objects: {
+      package: "0x4e58cae8dca973030dd84459bdf9cd70e65dcc80",
+      module: 'devnet_btc',
       TreasuryCap: '0x6ee60687c8d002ddc8efda046099f6dd96c9dc8c',
     },
     extensions: {
@@ -47,8 +51,9 @@ export const coinlist: CoinProfile[] = [
     officialSymbol: 'ETH',
     decimals: 8,
     logoURI: 'https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880',
-    package: "0x4e58cae8dca973030dd84459bdf9cd70e65dcc80",
     objects: {
+      package: "0x4e58cae8dca973030dd84459bdf9cd70e65dcc80",
+      module: 'devnet_eth',
       TreasuryCap: '0x5f43d1573427e99b5469b19aa1fa97f5ba133090',
     },
     extensions: {
@@ -62,8 +67,9 @@ export const coinlist: CoinProfile[] = [
     officialSymbol: 'USDC',
     decimals: 6,
     logoURI: 'https://assets.coingecko.com/coins/images/6319/small/USD_Coin_icon.png?1547042389',
-    package: "0x4e58cae8dca973030dd84459bdf9cd70e65dcc80",
     objects: {
+      package: "0x4e58cae8dca973030dd84459bdf9cd70e65dcc80",
+      module: 'devnet_usdc',
       TreasuryCap: '0x65ff9577325ea7e86ad302bfa6a705d5848f81d2',
     },
     extensions: {
@@ -77,10 +83,10 @@ export const coinlist: CoinProfile[] = [
     officialSymbol: 'USDT',
     decimals: 6,
     logoURI: 'https://assets.coingecko.com/coins/images/325/small/Tether-logo.png?1598003707',
-    package: "0x4e58cae8dca973030dd84459bdf9cd70e65dcc80",
     objects: {
-      TreasuryCap:
-      '0x4f9b93092387458dd13e803ebd8e6a4cad308d8f',
+      package: "0x4e58cae8dca973030dd84459bdf9cd70e65dcc80",
+      module: 'devnet_usdt',
+      TreasuryCap: '0x4f9b93092387458dd13e803ebd8e6a4cad308d8f',
     },
     extensions: {
       coingeckoId: 'tether',
