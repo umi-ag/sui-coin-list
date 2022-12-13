51065fd642523f0d5f46c89daf4a47e85f7859b6
fuyutarow <fuyutarow@gmail.com>
2022-12-13 13:21:33 +0900

    Add typescript/scripts

---
+++ typescript/scripts/temp.ts
@@ -0,0 +1,21 @@
+
+
+
+const provider = new JsonRpcProvider();
+
+
+let objectIds = [
+  "0x13818406d43ad19b21ca3eb57f43ba28b8511f41",
+  "0x17e73fb17d8be475b4bc15d8eaa34f63094b431c",
+  "0x46c517408defaf5857dbaab82df0f38bd81f2969",
+  "0x796dcad517f0e0fae3abecbb89fc22e9f9af0431",
+  "0x98b3ae97843b782721ad9bded3503ea0e6776739",
+  "0x9a467cc3f965897ff6032c07fbf14803e430840b",
+  "0xb65aed1616b67bd169526dde702133e7159679c0",
+  "0xce9c72bb2c89b01fe4674076020fe48744a831fe",
+  "0xf486cb71c5b45580815d8aafc46ebf0b1a7260e5",
+]
+
+console.log(objectIds)
+
+
